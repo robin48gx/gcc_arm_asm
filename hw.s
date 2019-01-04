dfa9824ab95dcdc97dc4306afeb9e3aa0db1d57e
Robin Clark <robin48gx@gmail.com>
2019-01-04 09:18:06 +0000

    experiment to pass args to printf from asm

--- hw.s
+++ hw.s
@@ -10,8 +10,10 @@ asm_main:
     @ Load the argument and perform the call. This is like 'printf("...")' in C.
     ldr     r0, =message
     bl      printf
-    ldr     r0, =message
+    ldr     r0, =message2
+    push    {r0, r1}
     bl      printf
+    pop     {r0, r1}
 
     @ Exit from 'main'. This is like 'return 0' in C.
     mov     r0, #0    @ Return 0.
@@ -25,4 +27,6 @@ asm_main:
     @ automatically adds a NULL character termination.
 message:
     .asciz "Hello, world.\n"
+message2:
+    .asciz "arg 1 0x%X arg 2 0x%X.\n"
 
